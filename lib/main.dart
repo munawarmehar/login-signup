706ffd21fce3a278d8d311fd70758664b3b71953
munawarmehar <munawarlatif01@gmail.com>
2025-03-13 23:39:29 -0700

    this is third commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:my_first_pro_vs/login.dart';
 import 'package:my_first_pro_vs/register.dart';
 import 'package:my_first_pro_vs/profile.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? email = prefs.getString('email');
+  String? password = prefs.getString('password');
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: 'login',
+    initialRoute: (email != null && password != null) ? 'profile' : 'login', // Check if user is logged in
     routes: {'login': (context) => MyLogin(),
     'register': (context)=>MyRegister(),
       'profile': (context)=>ProfileScreen()
--- lib/profile.dart
+++ lib/profile.dart
@@ -23,8 +23,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
       email = prefs.getString('email') ?? 'No email found';
       password = prefs.getString('password') ?? 'No password found';
     });
-
 }
+// Function to log out
+  _logout() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('email');
+    await prefs.remove('password');
+    Navigator.pushReplacementNamed(context, 'login'); // Redirect to login after logout
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,6 +45,16 @@ class _ProfileScreenState extends State<ProfileScreen> {
             Text('Email: $email', style: TextStyle(fontSize: 18)),
             SizedBox(height: 10),
             Text('Password: $password', style: TextStyle(fontSize: 18)),
+            ElevatedButton(
+              onPressed: _logout,
+              child: Text('logout'),
+              style: ElevatedButton.styleFrom(
+                maximumSize: Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                )
+              ),
+            )
           ],
         ),
       ),
